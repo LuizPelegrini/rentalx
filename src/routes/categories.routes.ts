461049faa03094c4012cfde06bb58427c981cbdb
Luiz Guilherme <ldesouzapelegrini01@gmail.com>
2022-02-13 16:12:34 +0800

    add: create categories router

---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,25 @@
+/**
+ * Router for Category
+ */
+
+import { Router } from 'express';
+import { v4 as uuidv4 } from 'uuid';
+
+const categoriesRouter = Router();
+const categories = [];
+
+categoriesRouter.post('/', (request, response) => {
+  const { name, description } = request.body;
+
+  const category = {
+    id: uuidv4(),
+    name,
+    description,
+  };
+
+  categories.push(category);
+
+  return response.status(201).send();
+});
+
+export { categoriesRouter };
