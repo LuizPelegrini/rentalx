dab491e42b2e3abb1ab70d79784a0660a3352ecc
Luiz Guilherme <ldesouzapelegrini01@gmail.com>
2022-02-19 15:51:14 +0800

    update: category name must be unique

--- src/repositories/CategoriesRepository.ts
+++ src/repositories/CategoriesRepository.ts
@@ -22,6 +22,7 @@ class CategoriesRepository {
    */
   create({ name, description }: ICreateCategoryDTO): void {
     const category = new Category();
+
     Object.assign(category, {
       name,
       description,
@@ -32,6 +33,11 @@ class CategoriesRepository {
 
   list(): Category[] {
     return this.categories;
+  }
+
+  findByName(name: string): Category {
+    const category = this.categories.find((category) => category.name === name);
+    return category;
   }
 }
 
