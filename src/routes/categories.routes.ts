7ab9702ae7770517b937558aba04c1f6006868b3
Luiz Guilherme <ldesouzapelegrini01@gmail.com>
2022-02-19 17:11:57 +0800

    add: CreateCategoryService to apply SRP and DIP principle

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -5,6 +5,7 @@
 import { Router } from 'express';
 
 import CategoriesRepository from '../repositories/CategoriesRepository';
+import CreateCategoryService from '../services/CreateCategoryService';
 
 const categoriesRouter = Router();
 const categoriesRepository = new CategoriesRepository();
@@ -12,13 +13,9 @@ const categoriesRepository = new CategoriesRepository();
 categoriesRouter.post('/', (request, response) => {
   const { name, description } = request.body;
 
-  // Category name must be unique
-  const categoryAlreadyExists = categoriesRepository.findByName(name);
-  if (categoryAlreadyExists) {
-    return response.status(400).json({ error: 'Category already exists!' });
-  }
-
-  categoriesRepository.create({ name, description });
+  // Dependency Inversion Principle
+  const createCategoryService = new CreateCategoryService(categoriesRepository);
+  createCategoryService.execute({ name, description });
 
   return response.status(201).send();
 });
